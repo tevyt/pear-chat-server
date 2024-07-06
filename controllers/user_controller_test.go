0d7cb781b7110d970e980fa8e5e3b80a35262d37
Travis Smith <travis.a.smith.ja@gmail.com>
2024-07-06 14:13:03 -0400

    Writes test for login.

--- controllers/user_controller_test.go
+++ controllers/user_controller_test.go
@@ -13,14 +13,26 @@ import (
 	"tevyt.io/pear-chat/server/dto"
 )
 
-type UserServiceDummy struct{}
+type UserServiceMock struct {
+	user dto.User
+}
 
-func (userService *UserServiceDummy) RegisterUser(dto.User) error {
+func (userService *UserServiceMock) RegisterUser(user dto.User) error {
+	userService.user = user
 	return nil
 }
 
+func (userService *UserServiceMock) Login(user dto.User) (dto.LoginSuccess, error) {
+	if userService.user.EmailAddress == user.EmailAddress {
+		return dto.LoginSuccess{EmailAddress: user.EmailAddress, SessionID: "123"}, nil
+	}
+
+	return dto.LoginSuccess{}, errors.New("Unable to login")
+}
+
 func Test200ResponseWhenUserRegisteredSuccessfully(t *testing.T) {
-	userController := NewUserController(&UserServiceDummy{})
+	userServiceMock := &UserServiceMock{}
+	userController := NewUserController(userServiceMock)
 
 	httpRecorder := httptest.NewRecorder()
 	context, _ := gin.CreateTestContext(httpRecorder)
@@ -38,30 +50,79 @@ func Test200ResponseWhenUserRegisteredSuccessfully(t *testing.T) {
 	if context.Writer.Status() != 200 {
 		t.Errorf("Register user responded with status: %d, expected: %d", context.Writer.Status(), 200)
 	}
+
+	if userServiceMock.user.EmailAddress != "test@test.com" {
+		t.Error("Expected registered email address")
+	}
+
 }
 
-type UserServiceMock struct{}
+type UserServiceErrorMock struct{}
 
-func (userService *UserServiceMock) RegisterUser(dto.User) error {
+func (userService *UserServiceErrorMock) RegisterUser(dto.User) error {
 	return errors.New("Mock error")
 }
+
+func (userService *UserServiceErrorMock) Login(dto.User) (dto.LoginSuccess, error) {
+	return dto.LoginSuccess{}, errors.New("Error in login.")
+}
+
 func Test500IfUserServiceReturnsAnError(t *testing.T) {
-	userController := NewUserController(&UserServiceMock{})
+	userController := NewUserController(&UserServiceErrorMock{})
 
-	httpRecorder := httptest.NewRecorder()
-	context, _ := gin.CreateTestContext(httpRecorder)
-	context.Request = &http.Request{Header: make(http.Header)}
-
-	context.Request.Method = "POST"
-	context.Request.Header.Set("content-type", "application/json")
-
-	requestBody, _ := json.Marshal(dto.User{Name: "Test", EmailAddress: "test@test.com", Password: "password123", PublicKey: "123"})
-
-	context.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+	context := buildGinContext(dto.User{Name: "Test", EmailAddress: "test@test.com", Password: "password123", PublicKey: "123"})
 
 	userController.RegisterUser(context)
 
 	if context.Writer.Status() != 500 {
 		t.Errorf("Register user responded with status: %d, expected: %d", context.Writer.Status(), 500)
 	}
+}
+
+func TestLogin200IfSuccessful(t *testing.T) {
+	userServiceMock := buildUserServiceMock()
+	userController := NewUserController(userServiceMock)
+
+	context := buildGinContext(dto.User{Name: "Test", EmailAddress: "test@test.com", Password: "password123", PublicKey: "123"})
+
+	userController.Login(context)
+
+	if context.Writer.Status() != 200 {
+		t.Errorf("Register user responded with status: %d, expected: %d", context.Writer.Status(), 200)
+	}
+}
+
+func TestLoginUnsuccessful(t *testing.T) {
+	userServiceMock := buildUserServiceMock()
+	userController := NewUserController(userServiceMock)
+
+	context := buildGinContext(dto.User{EmailAddress: "fail@test.com", Password: "password123"})
+
+	userController.Login(context)
+
+	if context.Writer.Status() != 401 {
+		t.Error("Expected unauthorized status.")
+	}
+
+}
+
+func buildUserServiceMock() *UserServiceMock {
+	return &UserServiceMock{
+		user: dto.User{
+			EmailAddress: "test@test.com",
+			Password:     "password123",
+		},
+	}
+}
+
+func buildGinContext(user dto.User) *gin.Context {
+	httpRecorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(httpRecorder)
+	context.Request = &http.Request{Header: make(http.Header)}
+	requestBody, _ := json.Marshal(user)
+
+	context.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+
+	return context
+
 }
