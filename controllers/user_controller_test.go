5f58aa8a91321685f9bf0929322d7eb88bd27174
Travis Smith <travis.a.smith.ja@gmail.com>
2024-07-05 21:12:07 -0400

    Adds error test for user controller.

--- controllers/user_controller_test.go
+++ controllers/user_controller_test.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -36,5 +37,31 @@ func Test200ResponseWhenUserRegisteredSuccessfully(t *testing.T) {
 
 	if context.Writer.Status() != 200 {
 		t.Errorf("Register user responded with status: %d, expected: %d", context.Writer.Status(), 200)
+	}
+}
+
+type UserServiceMock struct{}
+
+func (userService *UserServiceMock) RegisterUser(dto.User) error {
+	return errors.New("Mock error")
+}
+func Test500IfUserServiceReturnsAnError(t *testing.T) {
+	userController := NewUserController(&UserServiceMock{})
+
+	httpRecorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(httpRecorder)
+	context.Request = &http.Request{Header: make(http.Header)}
+
+	context.Request.Method = "POST"
+	context.Request.Header.Set("content-type", "application/json")
+
+	requestBody, _ := json.Marshal(dto.User{Name: "Test", EmailAddress: "test@test.com", Password: "password123", PublicKey: "123"})
+
+	context.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+
+	userController.RegisterUser(context)
+
+	if context.Writer.Status() != 500 {
+		t.Errorf("Register user responded with status: %d, expected: %d", context.Writer.Status(), 500)
 	}
 }
